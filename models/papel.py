e338530caace056df9f7e8ce02048a7acfc35f9c
Michael Miller <mullerg4michael@gmail.com>
2023-01-27 20:25:31 -0300

    model validation with unit and integration tests

--- models/papel.py
+++ models/papel.py
@@ -1,4 +1,7 @@
+import re
 import ormar
+from pydantic import validator
+from sqlalchemy.sql.expression import table
 from config import database, metadata
 
 class Papel(ormar.Model):
@@ -11,3 +14,9 @@ class Papel(ormar.Model):
     nome: str = ormar.String(max_length=100)
     sigla: str = ormar.String(max_length=10)
     cnpj: str = ormar.String(max_length=20)
+
+    @validator('sigla')
+    def valida_formatacao_sigla(cls, v):
+        if not re.compile('^[A-Z]{4}[0-9]{1,2}$').match(v):
+            raise ValueError('A sigla do papel é inválida!')
+        return v
